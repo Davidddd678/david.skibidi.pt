c394503efa7b7d995dc3fa5c72a7bdf8f26344dd
boda <lauradsds09@outlook.com>
2026-01-24 12:11:43 +0000

    pages created

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+pages =['home', 'login', 'register', 'play']
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return"home"
+    return render_template("index.html", pages=pages)
 
 @app.route('/login')
 def login():
-    return"login"
+    return render_template("login.html", pages=pages)
 
 @app.route('/register')
 def register():
-    return"register"
+    return render_template("register.html", pages=pages)
 
 @app.route('/play')
 def play():
-    return"play"
+    return render_template("play.html", pages=pages)
 
 app.run(debug=True, host='0.0.0.0')
 
