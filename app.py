ef0e94ed4f4714771127c029f8d9b9575b6da86e
AssemblyAdmin <ivan.rupt@gmail.com>
2026-02-07 12:05:31 +0000

    login e register

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+usuarios = []
+
+
 pages =['home', 'login', 'register', 'play']
 
 @app.route('/')
@@ -12,24 +15,44 @@ def home():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        name = request.form['name']
+        username = request.form['username']
         password = request.form['password']
 
-        print(name, password)
-        return "Conteudo enviado!"
+        for usuario in usuarios:
+            if usuario[0] == username and usuario[1] == password:
+                return render_template("index.html", pages=pages)
+
+        msg = " Goon não goontrado!"
+        return render_template("login.html",msg=msg)
+
     else:
-        return render_template("login.html", pages=pages)
+        return render_template("login.html")
 
 @app.route('/register', methods =['GET','POST'])
 def register():
-    if request.method =='POST':
-        name = request.form['name']
-        password = request.form ['password']
+    global usuarios
 
-        print(name,password)
-        return "Conteudo enviado!"
+    if request.method =='POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        print(username,password)
+
+        for usuario in usuarios:
+            if usuario[0] == username:
+                #return render_template("register.html", pages=pages)
+                msg = "Goon já"
+                return "Usuario existente"
+
+        usuarios.append((username,password))
+        msg ="Goon adiciogoon"
+        return render_template("register.html", msg= msg )
+
     else:
-        return render_template("register.html", pages=pages)
+        return render_template("register.html")
+
+
 
 @app.route('/play')
 def play():
