b13b71d8badb3a3a939965d32cae3672acb0eeb6
AssemblyAdmin <ivan.rupt@gmail.com>
2026-02-21 12:12:24 +0000

    login and register

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import psycopg
 app = Flask(__name__)
-app.secret_key = b'5$goongoonsahur\n\cex]/'
+app.secret_key = b'5$goongoonsahurncex]/'
 usuarios = []
 
-DB_URL = "postgresql://neondb_owner:npg_qGXk2mYF7ZyA@ep-delicate-pond-ab2yyzk8-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DB_URL = "postgresql://neondb_owner:npg_skLYJ5R9hIKe@ep-billowing-voice-abofarmr-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 conn = psycopg.connect(DB_URL)
 
 pages =['home', 'login', 'register', 'play']
@@ -17,7 +17,7 @@ def home():
         username = session['username']
         return render_template("index.html", username=username, pages=pages)
     else:
-        return render_template("index.html",  pages=pages)
+        return render_template("index.html")
 
 
 @app.route('/login', methods=['GET','POST'])
@@ -26,13 +26,20 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        for usuario in usuarios:
-            if usuario[0] == username and usuario[1] == password:
-                session['username'] = username
-                return render_template("index.html", pages=pages)
 
-        msg = " Goon não goontrado!"
-        return render_template("login.html",msg=msg)
+
+
+        crs = conn.cursor()
+        crs.execute('SELECT * FROM "Users" WHERE "Name" = %s AND "Password" = %s', (username, password))
+        user = crs.fetchone()
+
+        if user:
+             session ["username"] = user [1]
+             return render_template("index.html")
+        else:
+            msg ="Goonsário ou goonword ingoonretos"
+            return render_template("login.html",msg = msg)
+
 
     else:
         return render_template("login.html")
@@ -47,19 +54,21 @@ def register():
 
         username = request.form['username']
         password = request.form['password']
+        email = request.form['email']
 
         print(username,password)
 
-        for usuario in usuarios:
-            if usuario[0] == username:
-                #return render_template("register.html", pages=pages)
-                msg = "Goon já"
-                return "Usuario existente"
+        crs = conn.cursor()
+        crs.execute('SELECT 1 FROM "Users" WHERE "Name" = %s', (username,))
+        user = crs.fetchone()
 
-        usuarios.append((username,password))
+        if user:
+            msg=" Goon já goonxiste"
+            return render_template("register.html", msg= msg)
+
 
         crs = conn.cursor()
-        crs.execute("INSERT INTO msg (username, txt) VALUES (%s, %s)", (username, password))
+        crs.execute('INSERT INTO "Users" ("Name", "Email", "Password") VALUES (%s, %s, %s)', (username, email, password))
         conn.commit()
 
         msg ="Goon adiciogoon"
